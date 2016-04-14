f85093449e3f6cdc774180e21282ff45f2d7965a
秦先觉 <364103423@qq.com>
2016-04-14 16:04:47 +0800

    updated by qxj

---
+++ GitTest/GitTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitTest
+//
+//  Created by 秦先觉 on 16/4/14.
+//  Copyright © 2016年 Chong Maiyuan Technology Co., Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ GitTest/GitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by 秦先觉 on 16/4/14.
+//  Copyright © 2016年 Chong Maiyuan Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
